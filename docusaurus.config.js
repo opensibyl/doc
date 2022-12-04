fa9606fbe6164409d3910c7d5f70e825b5bcd7d6
williamfzc <178894043@qq.com>
2022-12-05 01:14:15 +0800

    first release ver

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,9 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark'
+      },
       navbar: {
         title: 'OpenSibyl',
         logo: {
